f9fd6e6e0b0cc6a9e2c6e0817b67d9ac4007a434
ghost-tremble <patokezy@gmail.com>
2022-04-11 10:32:44 +0100

    header icon placed

---
+++ src/pages/ProductPage.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import styled from 'styled';
+class ProductPage extends Component {
+  render() {
+    return <Container></Container>;
+  }
+}
+
+const Container = styled.div`
+  border: 1px solid;
+  height: 100vh;
+  width: 100%;
+  padding: 20px;
+`;
